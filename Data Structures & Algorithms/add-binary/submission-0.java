6a667229076efdce774a352374a98f47eaf9c8b7
bhavadharanimc-svg <bhavadharanimc@gmail.com>
2026-08-05 21:57:39 +0530

    Add: add-binary - submission-0

---
+++ Data Structures & Algorithms/add-binary/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        
+        int carry=0;
+        int i=a.length()-1;
+        int j=b.length()-1;
+        StringBuilder ans = new StringBuilder();
+        while(i>=0||j>=0||carry!=0){
+            int sum=carry;
+            if(i>=0){
+                sum+=a.charAt(i)-'0';
+                i--;
+            }
+            if(j>=0){
+                sum+=b.charAt(j)-'0';
+                j--;
+            }
+             ans.append(sum%2);
+             carry=sum/2;
+        }
+        return ans.reverse().toString();
+       
+    }
+}
